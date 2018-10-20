039be840ee23d62a76078275621d15fe74092a13
Julian <julianbrendl@gmail.com>
2018-10-20 19:20:10 +0200

    got multiprocessing to work

---
+++ src/debug.py
@@ -0,0 +1,7 @@
+from features import build_features
+from util import init
+
+# Init project
+init.init()
+
+build_features.build_img_data()
---
+++ src/util/init.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import locale
+
+def init():
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
