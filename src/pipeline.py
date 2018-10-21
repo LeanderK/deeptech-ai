4a2fca638d7d33acd1c255755bb62c8d88a54c02
Julian <julianbrendl@gmail.com>
2018-10-21 11:55:53 +0200

    added classes

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,7 +6,32 @@ from features.gather_seo_data import extract_metadata
 from features.score_images import get_keywords
 from features.quality_image_tags import filter_words
 from infersent import infersent
+import numpy as np
+import pickle
 
+labels = {
+    0: 'LAND- UND FORSTWIRTSCHAFT, FISCHEREI',
+    1: 'BERGBAU UND GEWINNUNG VON STEINEN UND ERDENBERGBAU UND GEWINNUNG VON STEINEN UND ERDEN',
+    2: 'ÖFFENTLICHE VERWALTUNG, VERTEIDIGUNG; SOZIALVERSICHERUNG',
+    3: 'KUNST, UNTERHALTUNG UND ERHOLUNG',
+    4: 'WASSERVERSORGUNG; ABWASSER- UND ABFALLENTSORGUNG UND BESEITIGUNG VON UMWELTVERSCHMUTZUNGEN',
+    5: 'GASTGEWERBE',
+    6: 'ENERGIEVERSORGUNG',
+    7: 'ERZIEHUNG UND UNTERRICHT',
+    8: 'GRUNDSTÜCKS- UND WOHNUNGSWESEN',
+    9: 'ERBRINGUNG VON SONSTIGEN DIENSTLEISTUNGEN',
+    10: 'ERBRINGUNG VON FINANZ- UND VERSICHERUNGSDIENSTLEISTUNGEN',
+    11: 'VERKEHR UND LAGEREI',
+    12: 'INFORMATION UND KOMMUNIKATION',
+    13: 'GESUNDHEITS- UND SOZIALWESEN',
+    14: 'BAUGEWERBE',
+    15: 'ERBRINGUNG VON SONSTIGEN WIRTSCHAFTLICHEN DIENSTLEISTUNGEN',
+    16: 'ERBRINGUNG VON FREIBERUFLICHEN, WISSENSCHAFTLICHEN UND TECHNISCHEN DIENSTLEISTUNGEN',
+    17: 'HANDEL; INSTANDHALTUNG UND REPARATUR VON KRAFTFAHRZEUGEN',
+    18: 'VERARBEITENDES GEWERBE'
+}
+
+from sklearn.externals import joblib
 def classify(url):
 
     # Get SEO keywords
@@ -22,6 +47,14 @@ def classify(url):
 
     # Calculate embeddings
     embeddings = list(map(infersent.get_embedding, keywords))
+    mean_embbedding = np.mean(embeddings, axis=0)
+
+    # Predict
+    model2 = joblib.load(r'02-SB-SVM.joblib')
+    clazz = model2.predict(mean_embbedding.reshape(1, -1))[0]
+    clazz = labels[clazz]
+    print('Predicted class: {cls}'.format(cls=clazz))
+    return clazz
 
 
-classify('www.opencodes.io')
+classify('www.hackundsoehne.de')
